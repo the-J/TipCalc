35700a0af1f5c37afa9ef5b98b04eb5479162dfe
the-j <asfalt09@gmail.com>
2018-06-21 22:56:15 +0200

    WIP handling tip as fixed value and as a custom value

--- tipcalc/App.js
+++ tipcalc/App.js
@@ -10,54 +10,99 @@ export default class App extends Component {
     this.state = {
       input: '',
       tip: 0,
-      percent: 0.2
+
+      tipValue: 0,
+      tipFixed: 0.2
     };
   }
 
+  setInput(input) {
+    if (input && input !== '0') {
+      this.setState({ input }, () => this.calculateTip());
+    } else {
+      this.setState({ input: '', tip: 0 });
+    }
+  }
+
+  setFixedTip(tipFixed) {
+    this.setState({ tipValue: tipFixed }, () => this.calculateTip());
+  }
+
+  setCustomTip(tipCustom) {
+    console.log('tipCustom', typeof tipCustom, tipCustom);
+    if (
+      tipCustom.length &&
+      tipCustom !== '0' &&
+      tipCustom.length < 4 &&
+      parseFloat(tipCustom) <= 100
+    ) {
+      this.setState({
+        tip: parseFloat(tipCustom) / 100,
+        tipValue: parseFloat(tipCustom) / 100
+      });
+    } else {
+      this.setState({
+        tip: 0,
+        tipValue: 0
+      });
+    }
+
+    this.calculateTip();
+  }
+
   calculateTip() {
-    if (this.state.input) {
-      tip = parseFloat(this.state.input) * this.state.percent;
+    console.log(
+      this.state.tipValue,
+      this.state.input,
+      typeof this.state.input,
+      this.state.input && this.state.tipValue !== 0
+    );
+
+    if (this.state.input.length) {
+      tip = parseFloat(this.state.input) * this.state.tipValue;
       tip = (Math.round(tip * 100) / 100).toFixed(2);
+
       this.setState({ tip });
+    } else {
+      this.setState({ tip: 0, input: '' });
     }
   }
 
   render() {
-    const { tip, percent } = this.state;
+    const { tip, tipValue } = this.state;
 
     return (
       <View style={styles.container}>
         <Text>
-          ${tip} / default: {percent}
+          ${tip} / tip: {tipValue} of the value
         </Text>
+
         <TextInput
           style={styles.input}
           value={this.state.input}
           keyboardType="numeric"
           placeholder="0.00"
-          onChangeText={value => {
-            this.setState({ input: value }, () => this.calculateTip());
-          }}
+          onChangeText={value => this.setInput(value)}
         />
+
         <View style={styles.buttonGroup}>
-          <Button
-            title="10%"
-            onPress={() =>
-              this.setState({ percent: 0.1 }, () => this.calculateTip())
+          <Button title="10%" onPress={() => this.setFixedTip(0.1)} />
+          <Button title="20%" onPress={() => this.setFixedTip(0.2)} />
+          <Button title="25%" onPress={() => this.setFixedTip(0.25)} />
+        </View>
+
+        <View style={styles.customTip}>
+          <TextInput
+            style={styles.customTipInput}
+            keyboardType="numeric"
+            placeholder="0.00"
+            value={
+              this.state.tipValue ? (this.state.tipValue * 100).toString() : ''
             }
+            onChangeText={tipCustom => this.setCustomTip(tipCustom)}
           />
-          <Button
-            title="20%"
-            onPress={() =>
-              this.setState({ percent: 0.2 }, () => this.calculateTip())
-            }
-          />
-          <Button
-            title="25%"
-            onPress={() =>
-              this.setState({ percent: 0.25 }, () => this.calculateTip())
-            }
-          />
+
+          <Text>%</Text>
         </View>
       </View>
     );
@@ -82,5 +127,16 @@ const styles = StyleSheet.create({
   },
   buttonGroup: {
     flexDirection: 'row'
+  },
+  customTip: {
+    flexDirection: 'row',
+    padding: 20
+  },
+  customTipInput: {
+    height: 30,
+    width: '60%',
+    borderColor: '#333',
+    borderWidth: 1,
+    padding: 5
   }
 });
